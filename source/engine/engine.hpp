fc67372ed25503acd04d78c8cb1715d0d84e078e
Tsvetelin Stoyanov <dexzoyp@gmail.com>
2023-02-04 12:03:30 +0200

    Linux compatibility

--- source/engine/engine.hpp
+++ source/engine/engine.hpp
@@ -1,7 +1,26 @@
 #pragma once
+#if defined(_MSC_VER)
+     //  Microsoft
 #ifdef Engine_EXPORTS
-    #define ENGINE_API __declspec(dllexport)
+     #define ENGINE_API __declspec(dllexport)
 #else
-    #define ENGINE_API __declspec(dllimport)
+     #define ENGINE_API __declspec(dllimport)
 #endif
-extern "C" ENGINE_API void HelloEngine();
+#elif defined(__GNUC__)
+     //  GCC
+#ifdef Engine_EXPORTS
+     #define ENGINE_API __attribute__((visibility("default")))
+#else
+     #define ENGINE_API
+#endif
+ #else
+     //  do nothing and hope for the best?
+#ifdef Engine_EXPORTS
+     #define ENGINE_API
+#else
+     #define ENGINE_API
+     #pragma warning Unknown dynamic link import/export semantics.
+#endif
+#endif
+ extern "C" ENGINE_API void HelloEngine();
+//extern void HelloEngine();
